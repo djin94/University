7eef54e1e8867e41a7d133bd3d542f18ba91caa2
evgenyk <djin94@yandex.ru>
2018-09-13 13:09:55 +0300

    Update

--- src/main/java/com/foxminded/university/console/Main.java
+++ src/main/java/com/foxminded/university/console/Main.java
@@ -13,7 +13,8 @@ public class Main {
         boolean stop = false;
         Scanner scanner = new Scanner(System.in);
         Main main = new Main();
-        University university = main.getRsreuUniversity();
+        UniversityModel universityModel = new UniversityModel();
+        University university = universityModel.getRsreuUniversity();
         MonthlySchedule monthlyScheduleForUniversity = main.getMonthlySchedyle();
         UniversityController universityController = new UniversityController();
         universityController.setUniversity(university);
@@ -85,6 +86,35 @@ public class Main {
         } while (!stop);
     }
 
+    private List<DailySchedule> getScheduleForUser(int choiceForWho, int choiceTypeSchedule) {
+
+    }
+
+    private Teacher getPersonFromConsole(University university) {
+        String lastName = getLastNameFromConsole();
+        String firstName = getFirstNameFromConsole();
+        String patronym = getPatronymFromConsole();
+        return universityController.getTeacherByName(lastName, firstName, patronym);
+    }
+
+    private String getLastNameFromConsole() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a last name");
+        return scanner.next();
+    }
+
+    private String getFirstNameFromConsole() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a first name");
+        return scanner.next();
+    }
+
+    private String getPatronymFromConsole() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a patronym");
+        return scanner.next();
+    }
+
     private void printSchedule(List<DailySchedule> dailySchedules) {
         dailySchedules.stream().flatMap(dailySchedule -> dailySchedule.getLessons().stream())
                 .peek(lesson -> {
@@ -100,127 +130,30 @@ public class Main {
                     System.out.println("Teacher: " + lesson.getTeacher().getLastName());
                 })
                 .forEach(lesson -> {
-                    System.out.println("Audience: " + lesson.getAudience().getNumber()+"\n");
-    });
+                    System.out.println("Audience: " + lesson.getAudience().getNumber() + "u" + lesson.getAudience().getBuilding() + "\n");
+                });
     }
 
-    private University getRsreuUniversity() {
-        Faculty faituFaculty = getFaituFaculty();
-        University university = new University();
-        university.setName("RSREU");
-        university.addFaculty(faituFaculty);
-        return university;
+    public DailySchedule getDailyScheduleForTeacher(Teacher teacher, int day) {
+        return monthlySchedule.getMonthlyScheduleForTeacher(teacher).stream()
+                .filter(dailySchedule -> dailySchedule.getDate().getDayOfMonth() == day).findFirst().get();
     }
 
-    private Faculty getFaituFaculty() {
-        Audience audience313 = getAudience313();
-        Department asuDepartment = getAsuDepartment();
-        Faculty faituFaculty = new Faculty();
-        faituFaculty.setName("FAITU");
-        faituFaculty.addAudience(audience313);
-        faituFaculty.addDepartment(asuDepartment);
-        return faituFaculty;
+    public DailySchedule getDailyScheduleForStudent(Student student, int day) {
+        return monthlySchedule.getMonthlyScheduleForStudent(student).stream()
+                .filter(dailySchedule -> dailySchedule.getDate().getDayOfMonth() == day).findFirst().get();
     }
 
-    private Audience getAudience313() {
-        Audience audience313 = new Audience();
-        audience313.setNumber(313);
-        audience313.setBuilding(1);
-        audience313.setType("Laboratory");
-        return audience313;
+    public DailySchedule getDailyScheduleForGroup(Group group, int day) {
+        return monthlySchedule.getMonthlyScheduleForGroup(group).stream()
+                .filter(dailySchedule -> dailySchedule.getDate().getDayOfMonth() == day).findFirst().get();
     }
 
-    private Department getAsuDepartment() {
-        Group group3033 = getGroup3033();
-        Teacher holopovTeacher = getHolopovTeacher();
-        Subject plisSubject = getPlisSubject();
-        Department asuDepartment = new Department();
-        asuDepartment.setName("ASU");
-        asuDepartment.addGroup(group3033);
-        asuDepartment.addTeacher(holopovTeacher);
-        asuDepartment.addSubject(plisSubject);
-        return asuDepartment;
+    public MonthlySchedule getMonthlySchedule() {
+        return monthlySchedule;
     }
 
-    private Group getGroup3033() {
-        Student kabatovStudent = getKabatovStudent();
-        Group group3033 = new Group();
-        group3033.setName("3033");
-        group3033.addStudent(kabatovStudent);
-        return group3033;
-    }
-
-    private Student getKabatovStudent() {
-        Student kabatovStudent = new Student();
-        kabatovStudent.setFirstName("Evgeny");
-        kabatovStudent.setPatronym("Nikolaevich");
-        kabatovStudent.setLastName("Kabatov");
-        kabatovStudent.setNumberOfMarkBook(230110);
-        return kabatovStudent;
-    }
-
-    private Teacher getHolopovTeacher() {
-        Subject plisSubject = getPlisSubject();
-        Teacher holopovTeacher = new Teacher();
-        holopovTeacher.setFirstName("Sergey");
-        holopovTeacher.setPatronym("Ivanovich");
-        holopovTeacher.setLastName("Holopov");
-        holopovTeacher.setEmployeeId(7852);
-        holopovTeacher.addSubject(plisSubject);
-        return holopovTeacher;
-    }
-
-    private Subject getPlisSubject() {
-        Subject plisSubject = new Subject();
-        plisSubject.setName("PLIS");
-        plisSubject.setHoursInSemestr(120);
-        return plisSubject;
-    }
-
-    private Subject getPoisSubject(){
-        Subject poisSubject = new Subject();
-        poisSubject.setName("POIS");
-        poisSubject.setHoursInSemestr(116);
-        return poisSubject;
-    }
-
-    private MonthlySchedule getMonthlySchedyle() {
-        Lesson plisLesson = getPlisLesson();
-        DailySchedule sep10DailySchedule = getSeptember10DailySchedule();
-        MonthlySchedule monthlyScheduleForUniversity = new MonthlySchedule();
-        monthlyScheduleForUniversity.setMonth(9);
-        monthlyScheduleForUniversity.setYear(2018);
-        monthlyScheduleForUniversity.addDailySchedule(sep10DailySchedule);
-        return monthlyScheduleForUniversity;
-    }
-
-    private DailySchedule getSeptember10DailySchedule() {
-        Lesson plisLesson = getPlisLesson();
-        DailySchedule sep10DailySchedule;
-        sep10DailySchedule = new DailySchedule();
-        sep10DailySchedule.setDate(LocalDate.of(2018, 9, 10));
-        sep10DailySchedule.addLesson(plisLesson);
-        sep10DailySchedule.addLesson(getPoisLesson());
-        return sep10DailySchedule;
-    }
-
-    private Lesson getPlisLesson() {
-        Lesson plisLesson = new Lesson();
-        plisLesson.setSubject(getPlisSubject());
-        plisLesson.setGroup(getGroup3033());
-        plisLesson.setTeacher(getHolopovTeacher());
-        plisLesson.setAudience(getAudience313());
-        plisLesson.setTimeStart(LocalTime.of(9, 55));
-        return plisLesson;
-    }
-
-    private Lesson getPoisLesson(){
-        Lesson poisLesson = new Lesson();
-        poisLesson.setSubject(getPoisSubject());
-        poisLesson.setGroup(getGroup3033());
-        poisLesson.setTeacher(getHolopovTeacher());
-        poisLesson.setAudience(getAudience313());
-        poisLesson.setTimeStart(LocalTime.of(11, 40));
-        return poisLesson;
+    public void setMonthlySchedule(MonthlySchedule monthlySchedule) {
+        this.monthlySchedule = monthlySchedule;
     }
 }
